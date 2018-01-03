00dcde9e883b93884055c765f8b9393b4c2af9f3
Niennis <estefania.osses.v@gmail>
2018-01-03 15:30:02 -0300

    Corrección en modales

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,6 +42,11 @@ $(document).ready(function(){
     $('h1').show();
   })
 
-
+//MODAL
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+    
 });   
   
