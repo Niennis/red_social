dcc85ec237b51c19ab0be951cf795e70da08132d
Niennis <estefania.osses.v@gmail>
2018-01-03 14:50:43 -0300

    Agregando newsfeed y posts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,48 @@
 $(document).ready(function(){ 
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });   
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA11xvSCkk7JLjgqr7HlrvPuNiW1yRydok",
+    authDomain: "labcraft-market.firebaseapp.com",
+    databaseURL: "https://labcraft-market.firebaseio.com",
+    projectId: "labcraft-market",
+    storageBucket: "labcraft-market.appspot.com",
+    messagingSenderId: "657357250509"
+  };
+  firebase.initializeApp(config);
   
 
+  //Para subir fotos
+  var fileBtn = $('fileBtn');
+
+  fileBtn.change(function(e){
+    var file = e.target.files[0];
+    var storageRef = firebase.storage().ref('mis_fotos/' + file.name);
+    var task = storageRef.put(file);
+  });
+
+  //codigo a ejecutar
+
+  $('#btn').click(function(){
+    var comentario = $('#comment').val();
+    $('#comment').val("");
+    var img = $('#fileBtn').val();
+    
+    var contenedor = $('#post');
+    contenedor.append('<div class="row"><div class="col s12 m6 l6 offset-m3 offset-l3">' + img + '<p>' + comentario + '</p><i class= "fa fa-trash trash"></i><i class = "fa fa-heart heart"></i></div><div>');
+  })
+
+  $('#hide').click(function(){
+    //$('#show').before('<p>Prueba</p>');
+    $('h1').hide();
+  })
+  $('#show').click(function(){
+    //$('#show').before('<p>Prueba</p>');
+    $('h1').show();
+  })
 
 
-
-
-//inicializaciones materialize//
-$(".button-collapse").sideNav();
-
-// Initialize collapse button
-
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
-
+});   
+  
