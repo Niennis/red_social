356316734c50b99996c62fae976daa5b0a41beee
Niennis <estefania.osses.v@gmail>
2018-01-03 22:18:20 -0300

    Agregando imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){ 
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
 
   // Initialize Firebase
   var config = {
@@ -58,24 +56,28 @@ $(document).ready(function(){
     }
   });
 
-
   //Para subir fotos
-  var fileBtn = $('fileBtn');
+  var fileBtn = $('#fileBtn');
 
+  var path="";
   fileBtn.change(function(e){
-    var file = e.target.files[0];
-    var storageRef = firebase.storage().ref('mis_fotos/' + file.name);
-    var task = storageRef.put(file);
+    var file = e.target.files[0];        
+    var storageRef = firebase.storage().ref();
+    var spaceRef = storageRef.child('mis_fotos/' + file.name);
+    storageRef.child('mis_fotos/' + file.name).getDownloadURL().then(function(url){
+      path = url;
+    }).catch(function(error) {
+
+    });
   });
 
   //Para crear un post
   $('#btn').click(function(){
     var comentario = $('#comment').val();
     $('#comment').val("");
-    var img = $('#fileBtn').val();
-    
+      
     var contenedor = $('#post');
-    contenedor.append('<div class="row"><div class="col s12 m6 l6 offset-m3 offset-l3">' + img + '<p>' + comentario + '</p><i class= "fa fa-trash trash"></i><i class = "fa fa-heart heart"></i></div><div>');
+    contenedor.append('<div class="row"><div class="col s12 m6 l6 offset-m3 offset-l3">' + '<img class="uploadedImg" src="' + path + '" alt=" ">' + '<p>' + comentario + '</p><i class= "fa fa-trash trash"></i><i class = "fa fa-heart heart"></i></div><div>');
   })
 
   $('#hide').click(function(){
